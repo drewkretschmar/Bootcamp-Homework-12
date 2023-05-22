a2670856f5229448889b0450cb6ff3a252fcd6f3
Drew <drewkretschmar@yahoo.com>
2023-05-22 16:05:53 -0400

    started adding data into seeds.sql

---
+++ Application/db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO departments (name)
+VALUES (Wood),
+       (Tile),
+       (Stone),
+       (Deco);
+       (Installation Materials);
+       (Pro Services);
+
+INSERT INTO roles (title, salary)
+VALUES  (Floor associate, 15),
+        (ADM, 18),
+        (DM 22),
+        (OPS, 30),
+        (CEM, 50);
+
+INSERT INTO employees (first_name, last_name)
+VALUES  (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        ();
