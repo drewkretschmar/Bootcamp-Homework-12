d78a25019a6daa4961c5db767bb240fb442f5510
Drew <drewkretschmar@yahoo.com>
2023-05-25 17:42:36 -0400

    corrected sql syntax, started inquirer

--- Application/db/schema.sql
+++ Application/db/schema.sql
@@ -4,27 +4,27 @@ CREATE DATABASE tables_db;
 USE tables_db;
 
 CREATE TABLE departments (
-    id: INT AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(30)/*hold department name*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id: INT AUTO_INCREMENT PRIMARY KEY,
-    title: VARCHAR(30),/*hold role title*/
-    salary: DECIMAL,/*hold role salary*/
-    department_id: INT,/*hold reference to department role belong to*/
-    FOREIGN KEY (departments)
-    REFERENCES (departments_id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id: INT AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(30),/*hold employee first name*/
-    last_name: VARCHAR(30),/*hold employee last name*/
-    role_id: INT,/*hold reference to employee role*/
-    manager_id: INT/*hold reference to another employee who is the manager of current employee (null if no manager)*/
-    FOREIGN KEY (roles)
-    REFERENCES(roles_title)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
     ON DELETE SET NULL
 );
--- Application/db/seeds.sql
+++ Application/db/seeds.sql
@@ -1,32 +1,32 @@
 INSERT INTO departments (name)
-VALUES (Wood),
-       (Tile),
-       (Stone),
-       (Deco);
-       (Installation Materials);
-       (Pro Services);
+VALUES ("Wood"),
+       ("Tile"),
+       ("Stone"),
+       ("Deco"),
+       ("Installation Materials"),
+       ("Pro Services");
 
 INSERT INTO roles (title, salary)
-VALUES  (Floor associate, 15),
-        (ADM, 18),
-        (DM 22),
-        (OPS, 30),
-        (CEM, 50);
+VALUES  ("Floor associate", 15),
+        ("ADM", 18),
+        ("DM", 22),
+        ("OPs", 30),
+        ("CEM", 50);
 
 INSERT INTO employees (first_name, last_name)
-VALUES  (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        ();
+VALUES  ("Billy", "Bob"),
+        ("Walter", "White"),
+        ("Jeff", "Gordon"),
+        ("Caitlyn", "Jenner"),
+        ("Josh", "Hamilton"),
+        ("Justin", "Bieber"),
+        ("Adolis", "Garcia"),
+        ("Peyton", "Manning"),
+        ("Sean", "O'Malley"),
+        ("Gary", "Patterson"),
+        ("Meredith", "Grey"),
+        ("Danica", "Patrick"),
+        ("Conor","McGregor"),
+        ("Skylar","White"),
+        ("Leo","Messi"),
+        ("Lebron","James");
